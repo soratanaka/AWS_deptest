a4b7428a130f22978ae0eb02cc22f4fae8fae1c1
sora tanaka <rari4423@gmail.com>
2022-09-30 15:43:43 +0900

    system add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,56 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice:"ブログを削除しました！"
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
